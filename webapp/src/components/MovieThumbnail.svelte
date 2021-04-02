9d724fff4f07fada75f3b7e72d351a9ce8dff819
Ezri Daniel Harmusial <ezriharmusial@gmail.com>
2021-04-02 02:10:46 +0200

    Movies Refactor -> MovieThumbnail

---
+++ webapp/src/components/MovieThumbnail.svelte
@@ -0,0 +1,26 @@
+<script>
+  import { timeConvert } from '../utils'
+
+  export let image = "/images/movies/small/mandelorian.jpg";
+  export let title = "Mandelorian";
+  export let length = "7600"
+</script>
+
+<style lang="scss"> 
+  figure {
+    position: relative;
+    width: 128px;
+
+    figcaption {
+      position: absolute;
+      bottom: 5px;
+      right: 5px;
+      z-index: 2;
+    }
+  }
+</style>
+
+<figure class="image">
+  <img src="{ image }" alt="poster { title }" />
+  <figcaption class="tag">{ timeConvert( length ) }</figcaption>
+</figure>
--- webapp/src/routes/Movies.svelte
+++ webapp/src/routes/Movies.svelte
@@ -1,7 +1,12 @@
 <script>
-  import { operationStore, query } from "@urql/svelte";
-  import {  timeConvert } from '../utils.js';
+  import MovieThumbnail from '../components/MovieThumbnail.svelte'
+  import { operationStore, query } from "@urql/svelte"
+  import { link } from 'svelte-spa-router'
 
+  // Accept incoming Parameters
+  export let params
+
+  // graphQL query
   const movies = operationStore(`
     {
       queryMovie {
@@ -16,23 +21,15 @@
       }
     }
   `);
-  query(movies);
 
+  // Execute query
+  query(movies)
+
+  // DevLogs
+  $: console.log("Page Params:", params)
+  $: console.log("Movies Data:", $movies.data?.queryActor)
 </script>
 
-<style lang="scss"> 
-  figure {
-    position: relative;
-    width: 128px;
-
-    figcaption {
-      position: absolute;
-      bottom: 5px;
-      right: 5px;
-      z-index: 2;
-    }
-  }
-</style>
 
 <div class="container">
   <h2 class="title mx-4">
@@ -49,16 +46,12 @@
   <div class="tile is-parent">
     {#each $movies.data.queryMovie as movie}
     <div class="tile notification is-flex is-flex-direction-row mx-4">
-      <figure class="image">
-        <img src="/images/movies/small/mandelorian.jpg" alt="poster { movie.title }" />
-        <figcaption class="tag">{ timeConvert(movie.length) }</figcaption>
-      </figure>
+      <MovieThumbnail title={movie.title} length={movie.length} />
       <div class="conten mx-3">
         <h3 class="title is-4">{movie.title}</h3>
         <p>
-        Cast<br />
-        {#each movie.actors as actor} <a class="mr-2" href="">{actor.name}</a> {/each}
-
+          Cast<br />
+          {#each movie.actors as actor} <a class="mr-2" href="/actors/{ actor.id }" use:link>{actor.name}</a> {/each}
         </p>
       </div>
     </div>
